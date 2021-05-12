1de4e5f9a2530dd5ac338a530724e7cd80ce4701
Joe <modulusman@gmail.com>
2021-05-12 13:19:59 -0700

    Updates meta tag information in gatsby-config.
    Also deletes boilerplate typescript file and second page.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Webstacks Coding Challenge`,
+    description: `Static site build using Gatsby, GraphQL, and Contentful.`,
     author: `@gatsbyjs`,
   },
   plugins: [
