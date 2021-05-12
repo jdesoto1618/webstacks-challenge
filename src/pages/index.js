6e20e1b82a383e0c5031e863ae7cca3371be2ecd
Joe <modulusman@gmail.com>
2021-05-12 14:10:27 -0700

    Adds SCSS to the project, with scss files to hold project styling.
    More files to be added later for component styling.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import * as React from "react"
 import { StaticImage } from "gatsby-plugin-image"
+import '../stylesheets/app.css';
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
