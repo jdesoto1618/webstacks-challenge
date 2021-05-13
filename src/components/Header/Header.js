f770b0dfe9c68dfc41910bcd5033b0eb32a40532
Joe <modulusman@gmail.com>
2021-05-13 16:40:27 -0700

    Adds a mobile menu which is hidden on desktop resolutions.
    Applies margins to hero tagline containers instead of the tagline itself.
    This fixes a layout issue on mobile.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,28 +1,36 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import { HiOutlineMenuAlt3 } from 'react-icons/hi'
 import * as headerStyles from "../../styles/header.module.css"
 
-export default function Header() {
+const Header = () => {
   return (
-    <nav className={headerStyles.navbar}>
-      <Link className={headerStyles.homeNavLink} to='/' />
-      <ul className={headerStyles.headerNavUl}>
-        <li className={headerStyles.headerNavLi}>
-          <Link className={headerStyles.navLink} to='/'>What's included?</Link>
-        </li>
-        <li className={headerStyles.headerNavLi}>
-          <Link className={headerStyles.navLink} to='/'>Pricing</Link>
-        </li>
-        <li className={headerStyles.headerNavLi}>
-          <Link className={headerStyles.navLink} to='/'>Sign In</Link>
-        </li>
-        <li className={headerStyles.headerNavLi}>
-          <Link className={headerStyles.navLink} to='/'>
-            <span className={headerStyles.getStarted}>Get started</span>
-            <span className={headerStyles.caretRight}>&gt;</span>
-          </Link>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <Link className={headerStyles.mobileNavContainer} to='/'>
+        <span className={headerStyles.mobileNavBars}></span>
+      </Link>
+      <nav className={headerStyles.navbar}>
+        <Link className={headerStyles.homeNavLink} to='/' />
+        <ul className={headerStyles.headerNavUl}>
+          <li className={headerStyles.headerNavLi}>
+            <Link className={headerStyles.navLink} to='/'>What's included?</Link>
+          </li>
+          <li className={headerStyles.headerNavLi}>
+            <Link className={headerStyles.navLink} to='/'>Pricing</Link>
+          </li>
+          <li className={headerStyles.headerNavLi}>
+            <Link className={headerStyles.navLink} to='/'>Sign In</Link>
+          </li>
+          <li className={headerStyles.headerNavLi}>
+            <Link className={headerStyles.navLink} to='/'>
+              <span className={headerStyles.getStarted}>Get started</span>
+              <span className={headerStyles.caretRight}>&gt;</span>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </>
   )
 }
+
+export default Header
