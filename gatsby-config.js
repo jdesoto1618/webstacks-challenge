8eba2620995f5353a58edb8123bb0870a8d5ec9d
Joe <modulusman@gmail.com>
2021-05-12 18:26:02 -0700

    Adds the rest of the nav links to the header.
    Adds padding to the site container div so that the hero will still
    be full width, but the gutters will exist on the sides.

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,6 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
     `gatsby-plugin-styled-components`,
-    `gatsby-plugin-sass`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,25 @@
 import * as React from "react"
 import { Link } from "gatsby"
-import { navbar, homeNavLink } from "../../styles/header.module.css"
+import * as headerStyles from "../../styles/header.module.css"
 
 export default function Header() {
   return (
-    <nav className={navbar}>
-      <Link className={homeNavLink} to='/' />
+    <nav className={headerStyles.navbar}>
+      <Link className={headerStyles.homeNavLink} to='/' />
+      <ul className={headerStyles.headerNavUl}>
+        <li>
+          <Link className={headerStyles.navLink} to='/'>What's included?</Link>
+        </li>
+        <li>
+          <Link className={headerStyles.navLink} to='/'>Pricing</Link>
+        </li>
+        <li>
+          <Link className={headerStyles.navLink} to='/'>Sign In</Link>
+        </li>
+        <li>
+          <Link className={headerStyles.navLink} to='/'>Get Started</Link>
+        </li>
+      </ul>
     </nav>
   )
 }
