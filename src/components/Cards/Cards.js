60aa425018aefdeaf4e25c7085026bd5d6452f3c
Joe <modulusman@gmail.com>
2021-05-13 11:13:56 -0700

    Adds the rest of the images onto the hero cards.
    Adds more styling and markup to the hero cards.

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,21 +1,48 @@
 import * as React from "react"
 import * as cardStyles from '../../styles/cards.module.css'
 import { Link } from "gatsby"
+import chopperImage from '../../images/chopper.png'
+import vrImage from '../../images/vr-headset.png'
+import mobileImage from '../../images/mobile-device.png'
 
 const Cards = () => {
   return (
     <>
       <div className={cardStyles.cardsContainer}>
         <div className={cardStyles.card}>
-
+          <img src={chopperImage} className={cardStyles.cardImage} />
+          <div className={cardStyles.cardTextContainer}>
+            <h5 className={cardStyles.cardTitle}>Augmented Reality</h5>
+            <p className={cardStyles.cardTagline}>An interactive experience like no other.</p>
+            <Link className={cardStyles.learnMore} to='/'>
+              <span className={cardStyles.ctaText}>Learn more</span>
+              <span className={cardStyles.caretRight}>&gt;</span>
+            </Link>
+          </div>
         </div>
 
         <div className={cardStyles.card}>
-
+          <img src={vrImage} className={cardStyles.cardImage} />
+          <div className={cardStyles.cardTextContainer}>
+            <h5 className={cardStyles.cardTitle}>Virtual Reality</h5>
+            <p className={cardStyles.cardTagline}>A truly immersive virutal experience.</p>
+            <Link className={cardStyles.learnMore} to='/'>
+              <span className={cardStyles.ctaText}>Learn more</span>
+              <span className={cardStyles.caretRight}>&gt;</span>
+            </Link>
+          </div>
         </div>
-        
-        <div className={cardStyles.card}>
 
+        <div className={cardStyles.card}>
+          <img src={mobileImage} className={cardStyles.cardImage} />
+          <div className={cardStyles.cardTextContainer}>
+            <h5 className={cardStyles.cardTitle}>Mobile</h5>
+            <p className={cardStyles.cardTagline}>Earn points on the go with our mobile app.</p>
+            <Link className={cardStyles.learnMore} to='/'>
+              <span className={cardStyles.ctaText}>Learn more</span>
+              <span className={cardStyles.caretRight}>&gt;</span>
+            </Link>
+          </div>
         </div>
       </div>
     </>
