d30808c964bae12dd857d547bfb91f7d937fed9f
Joe <modulusman@gmail.com>
2021-05-13 20:41:13 -0700

    Adds graphQL query to cards component.
    Corrects the images json entry for vr image.

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,11 +1,65 @@
 import * as React from "react"
 import * as cardStyles from '../../styles/cards.module.css'
 import { Link } from "gatsby"
+import { useStaticQuery, graphql } from 'gatsby'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import chopperImage from '../../images/chopper.png'
 import vrImage from '../../images/vr-headset.png'
 import mobileImage from '../../images/mobile-device.png'
 
 const Cards = () => {
+
+  const imageData = useStaticQuery(graphql`
+    query ImagesQuery {
+      allImagesJson {
+        edges {
+          node {
+            chopperImg {
+              childImageSharp {
+                fluid {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+            heroImg {
+              childImageSharp {
+                fluid {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+            mobileImg {
+              childImageSharp {
+                fluid {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+            vrImg {
+              childImageSharp {
+                fluid {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  function getImages(imageData) {
+    const imagesArray = []
+    imageData.allImagesJson.edges.forEach((image, index) => {
+      imagesArray.push(
+        <div key={index}>
+          <GatsbyImage src={image.node.chopperImage.childImageSharp.fluid.src} fluid={image.node.chopperImage.childImageSharp.fluid.src}></GatsbyImage>
+        </div>
+        
+      )
+    })
+  }
+
   return (
     <>
       <div className={cardStyles.cardsContainer}>
