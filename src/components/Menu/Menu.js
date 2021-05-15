5af35c17ce26db4940a746baa1b7ba65d51cf058
Joe <modulusman@gmail.com>
2021-05-15 10:51:28 -0700

    Sets mobile nav drawer to close when a mobile nav item is clicked.

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { StyledMenu } from './Menu.styled';
 import { Link } from "gatsby"
 
-const Menu = ({ isOpen }) => {
+const Menu = ({ isOpen, setIsOpen }) => {
   return (
-    <StyledMenu isOpen={isOpen}>
+    <StyledMenu isOpen={isOpen} onClick={() => setIsOpen(!isOpen)} className={isOpen ? 'opened' : ''}>
       <Link to='/'>What's included?</Link>
       <Link to='/'>Pricing</Link>
       <Link to='/'>Sign In</Link>
